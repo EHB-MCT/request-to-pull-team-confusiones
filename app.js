8357ea7ad24ab193087c94f44719c4cf4b421a89
RamzyMerad <ramzy.merad@student.ehb.be>
2021-10-15 12:31:39 +0200

    socketpart

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
-})
+  res.sendFile(__dirname + '/index.html');
+});
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
+
+
+server.listen(PORT, () => {
   console.log(`Express app listening at http://localhost:${PORT}`);
 })
